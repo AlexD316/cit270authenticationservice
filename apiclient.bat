527f86c2c2fa0e93684d7716935bb4e0b7d219ce
AlexD316 <alejandroduarteluz316@gmail.com>
2022-06-13 13:06:49 -0600

    Changed URL to custom one

--- apiclient.bat
+++ apiclient.bat
@@ -1,2 +1,2 @@
 ::Run for Windows
-curl --insecure -d "@data.json" -H "Content-Type:application/json" -X POST https://alex.cit270.com:4043/login
+curl -d "@data.json" -H "Content-Type:application/json" -X POST https://alex.cit270.com:4043/login
